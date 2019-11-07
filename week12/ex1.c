b7b0b37f6e23d84030d5b9207fa17598a2aca449
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2019-11-07 21:36:31 +0300

    Update ex1.c

--- week12/ex1.c
+++ week12/ex1.c
@@ -5,10 +5,10 @@
 char arr[N];
 int main() {
     int rnd = open("/dev/random", O_RDONLY);
-	int out = open("ex1.txt",O_WRONLY);
+    int out = open("ex1.txt",O_WRONLY);
     read(rnd,arr,N-1); 
-	arr[N-1]='\0';
-	write(out,arr,N-1);
+    arr[N-1]='\0';
+    write(out,arr,N-1);
     close(rnd);close(out);
     return 0;
 }
