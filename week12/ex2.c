b2e167033f38d4265ba29cb0488d2c425b682fa3
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2019-11-07 21:52:29 +0300

    Update ex2.c

--- week12/ex2.c
+++ week12/ex2.c
@@ -13,12 +13,12 @@ int main(int argc,char **argv){
 	
 	//S_IRWXG|S_IRWXO|S_IRWXU - user, group, others have/has read, write, and execute permission
 
-	if(append==0){//O_TRUNC will will be truncated to length 0
+	if(append==0){//O_TRUNC will be truncated to length 0
 		for(int i=1;i<argc;i++)
 			arr[pos++] = open(argv[i],O_TRUNC|O_APPEND|O_CREAT|O_WRONLY,S_IRWXG|S_IRWXO|S_IRWXU);
 	}
 	else{//command  -a
-		for(int i=1;i<argc;i++)
+		for(int i=2;i<argc;i++)
 			arr[pos++] = open(argv[i],O_APPEND|O_CREAT|O_WRONLY,S_IRWXG|S_IRWXO|S_IRWXU);
 	}
 	char c;
